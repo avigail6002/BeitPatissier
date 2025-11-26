287d1671193fe4ae1cf576ecfd827d75034461bc
DassiKastner <38215702531@mby.co.il>
2025-11-26 22:39:23 +0200

    Add relrvant DTOs for product

--- BeitPatissierServer/DTOs/ProductDTO.cs
+++ BeitPatissierServer/DTOs/ProductDTO.cs
@@ -1,17 +1,15 @@
-﻿using BeitPatissierServer.Models;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
 
 namespace BeitPatissierServer.DTOs
 {
     public class ProductDTO
     {
         public int ProductId { get; set; }
-        public string Name { get; set; } // שם המוצר
-        public string Description { get; set; } // תיאור המוצר
-        //public RecipeDTO Recipe { get; set; }
-        //public decimal Price { get; set; } // מחיר המוצר
-        //public List<ImageDTO> Images { get; set; } // כתובת התמונה של המוצר
-        //public List<ProductTagDTO> Tags { get; set; } //רשימת ארועים מתאימים
-        //public List<AllergenDTO> Allergens { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public RecipeDTO Recipe { get; set; }
+        public List<ImageDTO> Images { get; set; }
+        public List<ProductTagDTO> ProductTags { get; set; }
     }
 }
---
+++ BeitPatissierServer/DTOs/ProductTagDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BeitPatissierServer.DTOs
+{
+    public class ProductTagDTO
+    {
+        public int ProductId { get; set; }
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+    }
+}
+
