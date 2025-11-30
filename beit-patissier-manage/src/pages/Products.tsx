5bf5a9ac318680f59d180840a5050edac2d7c1d9
avigail6002 <avigail6002@gmail.com>
2025-11-30 14:09:56 +0200

    fix

--- beit-patissier-manage/src/pages/Products.tsx
+++ beit-patissier-manage/src/pages/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { GetAllProducts } from '../services/ProductService';
 import type { Product } from '../models/Product';
 import TableProduct from '../components/GenericTable';
